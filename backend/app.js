46509980055115c7d3d6fec9271613b6c54b2d86
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2022-12-04 01:21:58 +0530

    made changes

--- backend/app.js
+++ backend/app.js
@@ -28,10 +28,10 @@ const user = require("./routes/user");
 app.use("/api/v1",post);
 app.use("/api/v1",user);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 module.exports= app;
