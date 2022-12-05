bdc0ef6ff758139d51c6d4eabd2384925ec0c340
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2022-12-05 11:26:21 +0530

    listening after connection to the database

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,20 @@ const {connectDatabase} = require("./config/database");
 //cloudinary is a package to store image url's, we will use it to store the user's avatar
 const cloudinary = require("cloudinary");
 
-// This function mainly connects with the database
-connectDatabase();
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
 
-// listening to the environment variable PORT 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-});
+// This function mainly connects with the database
+connectDatabase()
+.then(()=>{
+    // listening to the environment variable PORT 
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+    console.log(err)
+})
