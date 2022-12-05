985fb968ce766d2da319c79d1b5b6419dd819d55
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2022-12-05 11:27:31 +0530

    async await while connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
-exports.connectDatabase = ()=>{
-    // MONGO_URI is the url for mongoDB database stored in the config.env
-    mongoose.connect(process.env.MONGO_URI)
-    .then(con=>console.log(`Database Connected: ${con.connection.host}`))
-    .catch((err)=>console.log(err))
+exports.connectDatabase = async ()=>{
+    try{
+        // MONGO_URI is the url for mongoDB database stored in the config.env
+        await mongoose.connect(process.env.MONGO_URI)
+        .then(con=>console.log(`Database Connected: ${con.connection.host}`))
+        .catch((err)=>console.log(err))
+    }
+    catch(err){
+        console.log(err)
+    }
 }
